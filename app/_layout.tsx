5560bb20ed95c8a45b88be3811dd46ced5dde6d1
aaron-lawless <aaronlawless1@gmail.com>
2025-11-28 16:32:33 -0600

    removed unused code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,9 @@
 import { Stack } from 'expo-router';
 import 'react-native-reanimated';
-
-import Header from '@/components/Header';
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import { DataProvider } from '@/context/DataContext';
 import { ThemeProvider, useTheme } from '@/context/ThemeContext';
 import '@/global.css';
-
-export const unstable_settings = {
-  initialRouteName: 'index',
-};
 
 function RootLayoutContent() {
   const { colorMode } = useTheme();
